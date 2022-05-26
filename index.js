e07b676b74e785f9cb0bf5a0a8b0408fb1da3016
Abdullayev Bahodir <bahodira213@gmail.com>
2022-05-26 14:59:48 +0500

    swiper nav

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var swiper = new Swiper(".swiper", {
   slidesPerView: 2,
-  spaceBetween: 8,
+  spaceBetween: 0,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   breakpoints: {
     768: {
       slidesPerView: 3,
